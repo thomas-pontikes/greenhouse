dcfe086497e1f41797126c2d887f06116f3ca973
kimholmgren <kimholmgren@uchicago.edu>
2017-07-12 10:22:41 -0400

    one lsat try

--- sb-admin-2.js
+++ sb-admin-2.js
@@ -1,3 +1,8 @@
+/*!
+ * Start Bootstrap - SB Admin 2 v3.3.7+1 (http://startbootstrap.com/template-overviews/sb-admin-2)
+ * Copyright 2013-2017 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
+ */
 $(function() {
     $('#side-menu').metisMenu();
 });
@@ -40,3 +45,66 @@ $(function() {
         }
     }
 });
+
+
+
+function addInput(divName) {
+    var newdiv = document.createElement('div');
+    newdiv.innerHTML =  '<input name="ticker[]" type="text" maxlength="10" id="tickerName"/><input name="percent[]" type="text" maxlength="10" id="percentage"/>';
+    document.getElementById(divName).appendChild(newdiv);
+}
+
+function readValues() {
+    var ticks = [];
+    var percents = [];
+    var descendants = document.getElementById("allocations").getElementsByTagName('*');
+    var ctr=0;
+    var i;
+    for(i=1; i<descendants.length; i+=3) {
+        ticks.push(descendants[i].value);
+    }
+    for(i=2; i<descendants.length; i+=3) {
+        percents.push(parseFloat(descendants[i].value));
+
+    }
+    return [ticks, percents];
+}
+
+function normalizePercents(percents) {
+    var sum = percents.reduce(function(a, b) { return a + b; }, 0);
+    return percents.map(function(e) { return e/sum; });
+
+}
+
+color = ["#f20909", "#fc8e07", "#f9f104", "#08ef04"];
+highlight = ["#f23232", "#f2a241", "#f9f340", "#4def4a"];
+
+function formatData(value, color, highlight, labels) {
+    var data = [];
+    for(var i=0; i<value.length; i++) {
+        curr = { value: value[i], color: color[i], highlight: highlight[i], label: labels[i] };
+        data.push(curr);
+    }
+    return data;
+
+}
+
+function myFunction() {
+    var res = readValues();
+    var ticks = res[0];
+    var percents = res[1];
+    var pcts = normalizePercents(percents);
+    document.getElementById("stockinfo").innerHTML=pcts;
+    var myCanvas = document.getElementById("piechart");
+    var ctx = myCanvas.getContext('2d');
+    var data = formatData(pcts, color, highlight, ticks);
+    console.log(data);
+    var option = {
+        responsive: true,
+        scaleShowLabels: true,
+        scaleLabel: "<%=value%>",
+        showTooltips: true
+    };
+    var myPieChart = new Chart(ctx).Pie(data, option);
+
+}
